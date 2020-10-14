bd27ee301c12820705da11035090d890483c41eb
Muhammad Ilyas <53975283+milyas26@users.noreply.github.com>
2020-10-14 23:26:39 +0700

    Install Firebase, membuat elemen header, dan install material UI.

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCU9Ert4pa863CCDpAq9Wb1fwKYRWJEwrI',
+  authDomain: 'clone-1da0e.firebaseapp.com',
+  databaseURL: 'https://clone-1da0e.firebaseio.com',
+  projectId: 'clone-1da0e',
+  storageBucket: 'clone-1da0e.appspot.com',
+  messagingSenderId: '799979909991',
+  appId: '1:799979909991:web:76216741ce9a749b2e75c2',
+  measurementId: 'G-Q67MF8SWEH',
+}
